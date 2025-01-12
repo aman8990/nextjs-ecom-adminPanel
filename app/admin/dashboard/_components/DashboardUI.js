02521171bc54525854004c57ca83c911c76286ec
Aman Kumar <amandalal899@gmail.com>
2025-01-12 21:54:34 +0530

    dashboard

--- app/admin/dashboard/_components/DashboardUI.js
+++ app/admin/dashboard/_components/DashboardUI.js
@@ -36,76 +36,85 @@ function DashboardUI() {
   } = useUserQueries();
 
   return (
-    <div className="grid gap-2 sm:gap-4 grid-cols-2 mt-32 mx-2 sm:mx-5">
-      <div
-        onClick={() => router.push('/admin/newOrders')}
-        className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
-      >
-        <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
-          <HiMiniRectangleStack />
+    <div>
+      <h1 className="text-center text-3xl md:text-4xl mb-14 mt-20">
+        Dashboard
+      </h1>
+      <div className="grid gap-2 sm:gap-4 grid-cols-2 mx-2 sm:mx-5">
+        <div
+          onClick={() => router.push('/admin/newOrders')}
+          className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
+        >
+          <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
+            <HiMiniRectangleStack />
+          </div>
+          <div>
+            <h1 className="text-sm md:text-base font-semibold text-gray-600">
+              New Orders
+            </h1>
+            <h1 className="text-xl font-bold text-gray-600">
+              {isLoadingNewOrders ? <SpinnerMini /> : newOrders?.length}
+            </h1>
+          </div>
         </div>
-        <div>
-          <h1 className="text-sm md:text-base font-semibold text-gray-600">
-            New Orders
-          </h1>
-          <h1 className="text-xl font-bold text-gray-600">
-            {isLoadingNewOrders ? <SpinnerMini /> : newOrders?.length}
-          </h1>
-        </div>
-      </div>
 
-      <div
-        onClick={() => router.push('/admin/acceptedOrders')}
-        className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
-      >
-        <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
-          <HiBadgeCheck />
+        <div
+          onClick={() => router.push('/admin/acceptedOrders')}
+          className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
+        >
+          <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
+            <HiBadgeCheck />
+          </div>
+          <div>
+            <h1 className="text-sm md:text-base font-semibold text-gray-600">
+              Accepted
+            </h1>
+            <h1 className="text-xl font-bold text-gray-600">
+              {isLoadingAcceptedOrders ? (
+                <SpinnerMini />
+              ) : (
+                acceptedOrders?.length
+              )}
+            </h1>
+          </div>
         </div>
-        <div>
-          <h1 className="text-sm md:text-base font-semibold text-gray-600">
-            Accepted
-          </h1>
-          <h1 className="text-xl font-bold text-gray-600">
-            {isLoadingAcceptedOrders ? <SpinnerMini /> : acceptedOrders?.length}
-          </h1>
-        </div>
-      </div>
 
-      <div
-        onClick={() => router.push('/admin/inTransitOrders')}
-        className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
-      >
-        <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
-          <HiTruck />
+        <div
+          onClick={() => router.push('/admin/inTransitOrders')}
+          className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
+        >
+          <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
+            <HiTruck />
+          </div>
+          <div>
+            <h1 className="text-sm md:text-base font-semibold text-gray-600">
+              InTransit
+            </h1>
+            <h1 className="text-xl font-bold text-gray-600">
+              {isLoadingInTransitOrders ? (
+                <SpinnerMini />
+              ) : (
+                inTransitOrders?.length
+              )}
+            </h1>
+          </div>
         </div>
-        <div>
-          <h1 className="text-sm md:text-base font-semibold text-gray-600">
-            InTransit
-          </h1>
-          <h1 className="text-xl font-bold text-gray-600">
-            {isLoadingInTransitOrders ? (
-              <SpinnerMini />
-            ) : (
-              inTransitOrders?.length
-            )}
-          </h1>
-        </div>
-      </div>
 
-      <div
-        onClick={() => router.push('/admin/userQueries')}
-        className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
-      >
-        <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
-          <HiAnnotation />
-        </div>
-        <div>
-          <h1 className="text-sm md:text-base font-semibold text-gray-600">
-            Queries
-          </h1>
-          <h1 className="text-xl font-bold text-gray-600">
-            {isLoadingUsersQueries ? <SpinnerMini /> : usersQueries?.length}
-          </h1>
+        <div
+          onClick={() => router.push('/admin/userQueries')}
+          className="flex items-center justify-center gap-2 sm:gap-4 p-2 sm:p-4 bg-white rounded-md cursor-pointer"
+        >
+          <div className="flex items-center justify-center text-white bg-green-700 rounded-full p-1 sm:p-2 text-xl sm:text-4xl md:text-3xl xl:text-6xl">
+            <HiAnnotation />
+          </div>
+          <div>
+            <h1 className="text-sm md:text-base font-semibold text-gray-600">
+              Queries
+            </h1>
+            <h1 className="text-xl font-bold text-gray-600">
+              {isLoadingUsersQueries ? <SpinnerMini /> : usersQueries?.length}
+            </h1>
+          </div>
         </div>
       </div>
     </div>
